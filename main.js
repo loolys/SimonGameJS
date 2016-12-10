95a3de5c613db6d7f9ef84e65589204ab40b7a32
loolys <pierre.liljekvist@outlook.com>
2016-12-10 11:07:35 +0100

    Added sound to the different buttons, made the game reset if you press the wrong button in the series

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 "use strict";
 
-var arr = [1, 2, 1, 2];
+var greenSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+var redSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+var yellowSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+var blueSound = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+
+var currentClick = 0;
+var arr = [];
 
 function game () {
     var random = Math.floor((Math.random() * 4) + 1);
@@ -9,27 +15,27 @@ function game () {
     for (let i = 0; i < arr.length; i++){
         if (arr[i] == 1){
             setTimeout( function timer(){
+                greenSound.play();
                 $("#green-button").css("background-color", "lightgreen");
                 setTimeout(changeColor, 500, "green");
-                console.log(i);
             }, i*1000);     
         } else if (arr[i] == 2) {
             setTimeout( function timer(){
+                redSound.play();
                 $("#red-button").css("background-color", "Tomato");
                 setTimeout(changeColor, 500, "red");
-                console.log(i);
             }, i*1000);    
         } else if (arr[i] == 3) {
             setTimeout( function timer(){
+                yellowSound.play();
                 $("#yellow-button").css("background-color", "lightyellow");
                 setTimeout(changeColor, 500, "yellow");
-                console.log(i);
             }, i*1000);    
         } else if (arr[i] == 4) {
             setTimeout( function timer(){
+                blueSound.play();
                 $("#blue-button").css("background-color", "Royalblue");
                 setTimeout(changeColor, 500, "blue");
-                console.log(i);
             }, i*1000);    
         }
     }
@@ -43,23 +49,96 @@ $(document).ready(function(){
     });
     
     $("#green-button").click(function(){
-        $("#green-button").css("background-color", "lightgreen");
-        setTimeout(changeColor, 500, "green");
+        if (arr[currentClick] != 1){
+            console.log("wrong");
+            arr = [];
+            currentClick = 0;
+            game();
+        } else{
+            
+            greenSound.play();
+            $("#green-button").css("background-color", "lightgreen");
+            setTimeout(changeColor, 500, "green");
+            currentClick += 1;
+            console.log(currentClick);
+            if (currentClick == arr.length){
+                setTimeout(function wait(){
+                    currentClick = 0;
+                    game();
+                }, 1000);
+                
+            }
+        }
+        
     });
     
     $("#red-button").click(function(){
-        $("#red-button").css("background-color", "Tomato");
-        setTimeout(changeColor, 500, "red");
+        if (arr[currentClick] != 2){
+            console.log("wrong");
+            arr = [];
+            currentClick = 0;
+            game();
+        } else{
+            redSound.play();
+            $("#red-button").css("background-color", "Tomato");
+            setTimeout(changeColor, 500, "red");
+            currentClick += 1;
+            console.log(currentClick);
+            if (currentClick == arr.length){
+                setTimeout(function wait(){
+                    currentClick = 0;
+                    game();
+                }, 1000);
+                
+            }
+        }
+        
     });
     
     $("#yellow-button").click(function(){
-        $("#yellow-button").css("background-color", "lightyellow");
-        setTimeout(changeColor, 500, "yellow");
+        if (arr[currentClick] != 3){
+            console.log("wrong");
+            arr = [];
+            currentClick = 0;
+            game();
+        } else{
+            yellowSound.play();
+            $("#yellow-button").css("background-color", "lightyellow");
+            setTimeout(changeColor, 500, "yellow");
+            currentClick += 1;
+            console.log(currentClick);
+            if (currentClick == arr.length){
+                setTimeout(function wait(){
+                    currentClick = 0;
+                    game();
+                }, 1000);
+                
+            }
+        }
+        
     });
     
     $("#blue-button").click(function(){
-        $("#blue-button").css("background-color", "RoyalBlue");
-        setTimeout(changeColor, 500, "blue");
+        if (arr[currentClick] != 4){
+            console.log("wrong");
+            arr = [];
+            currentClick = 0;
+            game();
+        } else{
+            blueSound.play();
+            $("#blue-button").css("background-color", "RoyalBlue");
+            setTimeout(changeColor, 500, "blue");
+            currentClick += 1;
+            console.log(currentClick);
+            if (currentClick == arr.length){
+                setTimeout(function wait(){
+                    currentClick = 0;
+                    game();
+                }, 1000);
+                
+            }
+        }
+        
     });
 });
 
